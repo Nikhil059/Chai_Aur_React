fb092f8e33accb40de11ab8b7e4ae7ee984797cf
Nikhil059 <99722876+Nikhil059@users.noreply.github.com>
2024-10-05 17:36:15 +0000

    AddRemoveProject

--- counter-project/src/App.js
+++ counter-project/src/App.js
@@ -1,20 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-let counterValue = 5;
+// let counterValue = 5;
 const AddValue = () =>{
-  counterValue = counterValue + 1;
+  if(counterValue == 20)
+    {
+      setcounterValue(counterValue = 20)
+    } 
+    else{
+      counterValue = counterValue + 1;
+      setcounterValue(counterValue)
   console.log(counterValue);
-  
-  
+    }
 }
+
+const DeleteValue = () => {
+  if(counterValue == 0)
+  {
+    setcounterValue(counterValue = 0)
+  }
+  else{
+    counterValue = counterValue - 1;
+  setcounterValue(counterValue)
+  console.log(counterValue);
+  }
+}
+let [counterValue,setcounterValue] = useState(10);
   return (
     <>
         <h1>Chai aur React</h1>
         <h2>Counter value : {counterValue}</h2>
         <br/>
         <button onClick={AddValue}>Add value</button><br/>
-        <button>Remove value</button>
+        <button onClick={DeleteValue}>Remove value</button>
     </>
   );
 }
