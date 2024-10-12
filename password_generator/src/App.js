e3064bb03ee46485d641fd4c70e30aad19c53363
Nikhil059 <99722876+Nikhil059@users.noreply.github.com>
2024-10-12 11:24:02 +0000

    change

--- password_generator/src/App.js
+++ password_generator/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -7,6 +7,15 @@ function App() {
   const[number,setNumber] = useState(false);
   const[character,setCharacter] = useState(false);
   const[password,setPassword] = useState();
+
+  const passwordGenerator = useCallback( () => {
+    pass = ""
+    str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(number) str += "0123456789"
+    if(character) str += "!@#$%^&*(){}[]`~-_+="
+
+    
+  },[lenght,number,character,setPassword])
   return (
     <>
       <h1 className='text-center text-white text-4xl m-4'>Password Generator</h1>
